c14d3fd94eb9d24f9110ba1ec1d888bfee6346bc
Musab-Al-omari <musabsomari@gmail.com>
2021-06-07 17:29:36 +0300

    hello

--- public/vedioScript.js
+++ public/vedioScript.js
@@ -1,14 +1,16 @@
 'use strict';
-
-
-
 const peerObj = {};
+let peerKeyArray = [];
 const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 const myVideo = document.createElement('video');
 const videoGrid = document.getElementById('video-grid');
 myVideo.muted = true;
+const shareScreenB = document.getElementById('shareScrean');
+
+
+
 
 
 
@@ -16,11 +18,9 @@ myVideo.muted = true;
 const socket = io('/');
 // eslint-disable-next-line no-undef
 const peer = new Peer();
-
+// eslint-disable-next-line no-undef
 
 // some peer configuration 
-
-
 // undefined, {
 //   host: 'vediotest-123.herokuapp.com',
 //   port: '3003',
@@ -30,42 +30,62 @@ const peer = new Peer();
 
 
 
-// let hostId = '';
+
 peer.on('open', (id) => {
   // eslint-disable-next-line no-undef
-  // hostId = id;
-  // console.log('this is my peer id', id);
+  console.log('peer id ', id);
+  peerKeyArray.push(id);
   // eslint-disable-next-line no-undef
   socket.emit('join-room', roomId, id);
 
 });
 
-const myNavigator = navigator.mediaDevices.getUserMedia({ video: true, audio: true }) || navigator.mediaDevices.webkitGetUserMedia({ video: true, audio: true }) || navigator.mediaDevices.mozGetUserMedia({ video: true, audio: true }) || navigator.mediaDevices.msGetUserMedia({ video: true, audio: true });
 
-async function shareScreen() {
-  return await navigator.mediaDevices.getDisplayMedia({ video: true });
-}
-
-
-
-
+// add video to your page
 const addVideoStream = async(video, stream) => {
-  console.log(2);
   video.srcObject = await stream;
-
   video.addEventListener('loadedmetadata', () => {
     video.play();
   });
   videoGrid.append(video);
 };
 
+
+
+
+async function callStream(stream) {
+  addVideoStream(myVideo, stream);
+  let x = await stream;
+
+  peer.on('call', call => {
+    call.answer(x);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream);
+    });
+
+    call.on('close', () => {
+      console.log('CLOSED');
+      video.remove();
+    });
+    // peerObj[userId] = call;
+  });
+  socket.on('user-connected', (userId) => {
+    console.log('first time ');
+    setTimeout(() => {
+      connectToNewUser(userId, x);
+    }, 5000);
+  });
+}
+const myNavigator = navigator.mediaDevices.getUserMedia({ video: true, audio: true }) || navigator.mediaDevices.webkitGetUserMedia({ video: true, audio: true }) || navigator.mediaDevices.mozGetUserMedia({ video: true, audio: true }) || navigator.mediaDevices.msGetUserMedia({ video: true, audio: true });
+callStream(myNavigator);
+
+
 const connectToNewUser = async(userId, stream) => {
   let x = await stream;
   const call = peer.call(userId, x);
-  console.log('hi');
-  const video = document.createElement('video');
 
-  console.log('call', call);
+  const video = document.createElement('video');
 
   call.on('stream', function(userVideoStream) {
     console.log('hiiii');
@@ -76,87 +96,48 @@ const connectToNewUser = async(userId, stream) => {
     console.log('CLOSED');
     video.remove();
   });
+  ////////////////////////////////////////////
 
+  // if (ks) {
+  //   peerObj[od] = call;
+  //   ks = false;
+  // } else {
   peerObj[userId] = call;
-
+  // }
   // function hostClose(userId) {
   //   peerObj[userId].close();
   // }
-  console.log(peerObj);
+  console.log('connect to new user', peerObj);
 };
 
-
-
-async function callStream(stream) {
-  console.log(1);
-  addVideoStream(myVideo, stream);
-  // addShareScreen((myVideo, stream));
-  let x = await stream;
-  peer.on('call', call => {
-
-    call.answer(x);
-    const video = document.createElement('video');
-    console.log(3);
-
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream);
-    });
-
-    call.on('close', () => {
-      console.log('CLOSED');
-      video.remove();
-    });
-
-    // peerObj[userId] = call;
-  });
-  socket.on('user-connected', (userId) => {
-    console.log('userId first', userId);
-    setTimeout(() => {
-      connectToNewUser(userId, stream);
-    }, 2000);
-  });
-}
-callStream(myNavigator);
-
-
-
-
-
-
-
-// const addShareScreen = (myVideo, stream) => {
-//   myVideo.srcObject = window.URL.createObjectURL(stream);
-//   myVideo.play();
-// };
-
-
-
-
-socket.on('connect_error', (err) => {
-  console.log(`connect_error due to ${err.message}`);
+// socket.client error handler 
+socket.on('connect_error', err => {
+  console.log(err instanceof Error); // true
+  console.log(`connect_error due to ${err.message}`); // not authorized
+  console.log(err.data); // { content: "Please retry later" }
 });
 
 
-
+// alert that a user joined to the browser       //////////////////////////////////////////////////////////////////////////
 socket.on('userId-Joined', userid => {
   let name = prompt('give me ur name');
   if (name === null) {
     name = 'black guy from Connan';
   }
-  // console.log(`user ${name} with the id ${userid} connect to the chat`);
   appendMessage(`user :${name} has recently joined`);
   socket.emit('sendUserToServer', name);
-
 });
+
+// append the user name to message container
 socket.on('sendBackUserToFrontEnd', name => {
   appendMessage(`user :${name} has recently joined`);
 });
 
+
+// when a user close the browser
 socket.on('user-disconnected', (name, id) => {
 
-  console.log('peerObj[id] dis', peerObj.id);
   peerObj[id].close();
-
   appendMessage(`user:${name} has been disconnected`);
 });
 
@@ -172,18 +153,16 @@ function eventSubmit(event) {
     socket.emit('sendMassageToServer', message);
     messageInput.value = '';
   }
-
 }
-
-
-
 
 // WRITE THE MESSAGE THAT CAME BACK FROM THE SERVER
 socket.on('sendMassageBackToFrontEnd', (message, name) => {
-  // let Isak = `${name}:${message}`;
   appendMessage(`${name}:${message}`);
 });
 
+
+
+// to append a message to the chat container
 function appendMessage(message) {
   const messageElement = document.createElement('section');
   messageElement.innerText = message;
@@ -192,45 +171,44 @@ function appendMessage(message) {
 
 
 
+//  share screen function 
+let share;
+async function shareScreen() {
+  return await navigator.mediaDevices.getDisplayMedia({ video: true });
+}
+
+shareScreenB.addEventListener('click', function name(event) {
+  event.preventDefault();
+  const video = document.createElement('video');
+  video.setAttribute('class', 'share');
+  video.muted;
+  share = shareScreen();
+  addVideoStream(video, share);
+  socket.emit('share');
+});
 
 
-
-// const shareScreen = async() => {
-//   let captureStream = null;
-
-//   try {
-//     captureStream = await navigator.mediaDevices.getDisplayMedia();
-//   } catch (err) {
-//     console.error("Error: " + err);
-//   }
-//   // connectToNewUser(myUserId, captureStream);
-//   peer.call(myUserId, captureStream);
-// };
+socket.on('user-share', (userId) => {
+  console.log('user-share', userId);
+  connectNewShare(userId, share);
+});
 
 
+async function connectNewShare(userId, stream) {
+  const call = peer.connect(userId, stream);
+  const video = document.createElement('video');
+  video.setAttribute('class', 'share');
+  console.log('call', call);
 
 
+  console.log('?');
+  call.on('stream', function(userVideoStream) {
+    console.log('gg');
+    addVideoStream(video, userVideoStream);
+  });
 
-
-
-
-
-
-
-
-
-
-// const myVideoElement = document.createElement('video');
-// myVideoElement.muted = true;
-// const videoGrid = document.getElementById('video-grid');
-
-// navigator.mediaDevices.getUserMedia({
-//   video: true,
-//   audio: true,
-// }).then((stream) => {
-//   myVideoElement.srcObject = stream;
-//   myVideoElement.addEventListener('loadedmetadata', () => {
-//     myVideoElement.play();
-//   });
-//   videoGrid.append(myVideoElement);
-// });
+  call.on('close', () => {
+    console.log('CLOSED');
+    video.remove();
+  });
+}
